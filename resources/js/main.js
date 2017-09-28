d9bff11501e4b7eb6e0dfaaf1c843660fdbae1b5
Dani <danihazler@gmail.com>
2017-09-28 13:21:48 +0100

    First commit

---
+++ resources/js/main.js
@@ -0,0 +1,47 @@
+$(document).on("scroll", function(){
+  var pixelsFromTop= $(document).scrollTop();
+
+// HIDDEN THE HEADER ---------------------------
+
+  if (pixelsFromTop > 50) {
+    $("header").addClass("hidden");
+  } else {
+    $("header").removeClass("hidden");
+  }
+
+// CHANGING BG COLOUR --------------------------
+  if (pixelsFromTop < 600) {
+    $("body").css("background-color", "#fff")
+  }
+  else if (pixelsFromTop < 1400) {
+    $("body").css("background-color", "#a29c97")
+  }
+  else if (pixelsFromTop < 2200) {
+    $("body").css("background-color", "#d9dfe4")
+  }
+  else if (pixelsFromTop < 3000) {
+    $("body").css("background-color", "#fff0f0")
+  }
+  else {
+    $("body").css("background-color", "#cdccc7")
+  }
+
+// PROGRESS BAR ---------------------------
+  var documentHeight = $(document).height();
+  var windowHeight = $(window).height();
+
+  var difference = documentHeight - windowHeight;
+
+  var percentage = 100 * pixelsFromTop / difference;
+
+   $(".bar").css("width", percentage + "%")
+});
+
+// OPENING THE RIGHT RECIPE ------------------------
+$('.open-recipe, .img-open').on('click', function () {
+  $(this).closest('.recipe-img').find('.modal').addClass('visible');
+});
+
+$('.modal-close, .modal-content').on('click', function () {
+  $(this).closest('.recipe').find('.modal').removeClass('visible');
+});
